ab37a7919108ea648432cc4a324933af4a0d5539
Mart_Tilda <denis02012000@hotmail.com>
2025-08-08 14:39:26 +0300

    Update cli.cpp

--- scripts/src/cli.cpp
+++ scripts/src/cli.cpp
@@ -8,8 +8,8 @@
 void CLI::run(){
     Exec executor;
     std::atomic<bool> running = true;
-    auto input_f = std::async(std::launch::async, [&](){ // асинхронная обработка потока ввода. Пускай пока что будет так, в будущем мб добавлю еще какие то обработки поверх этой
-        std::string command; // команда с потока ввода. Динамичная по дефолту класса string
+    auto input_f = std::async(std::launch::async, [&](){ // асинхронная обработка потока ввода
+        std::string command; 
         while(running){
             std::cout << "> ";
             if(!std::getline(std::cin, command)){
@@ -22,14 +22,9 @@ void CLI::run(){
                 break;
             }
             if(command.empty())  continue; // проверка на ENTER при пустой строке
-            // std::cin.get() == 1310 - такая проверка конечно имеет место быть, но она избыточна, так как empty проверяет на пустую строку
-            executor.exec_com(command); // далее уже запускается работа executor-а
+            // std::cin.get() == 1310 - избыточна
+            executor.exec_com(command);
         }
     });
-
-    /*Какое либо действие поверх
-    while(running){fmt::print("go suc");}
-    */
-
-    input_f.wait(); // завершение
+    input_f.wait(); 
 }
